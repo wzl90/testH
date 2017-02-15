0823a5730ec15aa21f218b7882f254db87ceaf67
wzl90 <wuzhuanlin@sina.com>
2017-02-15 15:50:47 +0800

    newPage

---
+++ jsTrick.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wuzhuanlin on 17/2/15.
+ */
+window.onload = function ()
+{
+    var btn = document.getElementById("button1");
+    btn.onclick = function ()
+    {
+        window.open("http://localhost:63342/TestH/testXXX.html");
+    }
+}
